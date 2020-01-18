7d2ea6346d774afb23018fffce7955c3627187c3
ana1155 <anastasiia.palashkevych@gmail.com>
2020-01-18 14:47:47 +0200

    job additional fields

--- migrations/20200118102041-create-job.js
+++ migrations/20200118102041-create-job.js
@@ -15,6 +15,42 @@ module.exports = {
     source: {
       type: Sequelize.STRING,
     },
+    link: {
+      type: Sequelize.STRING,
+    },
+    team: {
+      type: Sequelize.TEXT,
+    },
+    remoteOption: {
+      type: Sequelize.BOOLEAN,
+      field: 'remote_option',
+    },
+    fullyRemote: {
+      type: Sequelize.BOOLEAN,
+      field: 'fully_remote',
+    },
+    laptopProvided: {
+      type: Sequelize.BOOLEAN,
+      field: 'laptop_provided',
+    },
+    stack: {
+      type: Sequelize.TEXT,
+    },
+    officeAddress: {
+      type: Sequelize.STRING,
+      field: 'office_address',
+    },
+    additionalBonuses: {
+      type: Sequelize.TEXT,
+      field: 'additional_bonuses',
+    },
+    projectAge: {
+      type: Sequelize.INTEGER,
+      field: 'project_age',
+    },
+    comments: {
+      type: Sequelize.TEXT,
+    },
     createdAt: {
       allowNull: false,
       type: Sequelize.DATE,
--- server/job/job.model.js
+++ server/job/job.model.js
@@ -11,6 +11,42 @@ const JobModel = sequelize.define('job', {
   source: {
     type: Sequelize.STRING,
   },
+  link: {
+    type: Sequelize.STRING,
+  },
+  team: {
+    type: Sequelize.TEXT,
+  },
+  remoteOption: {
+    type: Sequelize.BOOLEAN,
+    field: 'remote_option',
+  },
+  fullyRemote: {
+    type: Sequelize.BOOLEAN,
+    field: 'fully_remote',
+  },
+  laptopProvided: {
+    type: Sequelize.BOOLEAN,
+    field: 'laptop_provided',
+  },
+  stack: {
+    type: Sequelize.TEXT,
+  },
+  officeAddress: {
+    type: Sequelize.STRING,
+    field: 'office_address',
+  },
+  additionalBonuses: {
+    type: Sequelize.TEXT,
+    field: 'additional_bonuses',
+  },
+  projectAge: {
+    type: Sequelize.INTEGER,
+    field: 'project_age',
+  },
+  comments: {
+    type: Sequelize.TEXT,
+  },
 }, {
   underscored: true,
 });
