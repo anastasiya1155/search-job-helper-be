001e84772066f9c698202332d803697fd13a2c87
ana1155 <anastasiia.palashkevych@gmail.com>
2020-01-18 12:15:01 +0200

    refactor index.js

---
+++ server/rootSchema.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+};
