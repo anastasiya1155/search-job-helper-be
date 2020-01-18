787996ff9b3cfe6dab34ca805c81bd91026fb6f6
ana1155 <anastasiia.palashkevych@gmail.com>
2020-01-18 14:09:37 +0200

    improve tests setup

--- server/utils/testUtils/db.js
+++ server/utils/testUtils/db.js
@@ -10,18 +10,24 @@ const cleanDB = async (done) => {
   done();
 };
 
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: 'sjh-test',
-});
+let sequelize;
 
-const connectToDB = () => sequelize;
+const connectToDB = () => {
+  sequelize = new Sequelize({
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'sjh-test',
+  });
+};
 
 const disconnectDB = async (done = () => {}) => {
-  sequelize.close().then(done);
+  if (sequelize) {
+    sequelize.close().then(done);
+  } else {
+    done();
+  }
 };
 
 module.exports = {
