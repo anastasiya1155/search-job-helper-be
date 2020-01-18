81b0edf5fac8f56b345b50b4c97578d08d5e8f72
ana1155 <anastasiia.palashkevych@gmail.com>
2020-01-18 14:32:33 +0200

    job mutations

--- server/job/job.resolvers.js
+++ server/job/job.resolvers.js
@@ -1,6 +1,17 @@
 module.exports = {
+  JobMutations: {
+    edit: async (parent, { input }) => await parent.update(input),
+    remove: async (parent, args, ctx) => {
+      await parent.destroy();
+      return await ctx.models.job.findAll();
+    },
+  },
   Query: {
     getAllJobs: async (parent, args, ctx) => await ctx.models.job.findAll(),
     getJobById: async (parent, { id }, ctx) => await ctx.models.job.findByPk(id),
+  },
+  Mutation: {
+    createJob: async (parent, { input }, ctx) => await ctx.models.job.create(input),
+    job: async (parent, { id }, ctx) => await ctx.models.job.findByPk(id),
   },
 };
--- server/job/job.spec.js
+++ server/job/job.spec.js
@@ -9,7 +9,7 @@ describe('Project', () => {
   afterEach(db.cleanDB);
 
   describe('resolvers', () => {
-    describe('job', () => {
+    describe('job queries', () => {
       test('should resolve correctly', async () => {
         const job = await db.models.job.create({ name: 'test' });
         const result = await jobResolvers.Query
@@ -33,6 +33,43 @@ describe('Project', () => {
 
         expect(errors).toBeUndefined();
         expect(data.getJobById.id).toBe(`${job.id}`);
+      });
+    });
+    describe('job mutations', () => {
+      test('should create job', async () => {
+        const input = { input: { name: 'test', position: 'tester', source: 'friend' } };
+        const result = await jobResolvers.Mutation.createJob(null, input, { models: db.models });
+
+        expect(result.name).toBe(input.input.name);
+      });
+      test('should edit job', async () => {
+        const job = await db.models.job.create({ name: 'test', position: 'tester', source: 'friend' });
+        const input = { id: job.id, input: { position: 'FullStack' } };
+        const query = `
+          mutation editJob($id: ID!, $input: JobInput!){
+            job(id: $id) {
+              edit(input: $input) {
+                id
+                name
+                position
+              }
+            }
+          }
+        `;
+        const { data, errors } = await runQuery(query, input);
+
+        expect(errors).toBeUndefined();
+        expect(data.job.edit.position).toBe(input.input.position);
+      });
+      test('should remove job', async () => {
+        await db.models.job.bulkCreate([
+          { name: 'test', position: 'tester', source: 'friend' },
+          { name: 'test 2', position: 'FE', source: 'Djiny' },
+        ]);
+        const jobs = await jobResolvers.Query.getAllJobs(null, {}, { models: db.models });
+        const result = await jobResolvers.JobMutations.remove(jobs[0], {}, { models: db.models });
+
+        expect(result.length).toBe(jobs.length - 1);
       });
     });
   });
